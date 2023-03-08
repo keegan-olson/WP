849e24640212c7d5841256282bd2a578599f1d6d
keegan-olson <76485182+keegan-olson@users.noreply.github.com>
2023-03-08 09:44:49 -0800

    changed a tags to Link

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { client, urlFor } from "../lib/sanity";
 import { groq } from "next-sanity";
+import Link from "next/link";
 
 export default function Home({ hero1Url, hero2Url, hero3Url }) {
   return (
     <div className='grid md:grid-cols-3 grid-cols-1 md:h-[80vh]  gap-3 mx-2 py-2 bg-black'>
-      <a href='/portfolio'><div
+      <Link href='/portfolio'><div
         style={{
           backgroundImage: `url(${`${hero1Url}?blur=50`})`,
           backgroundSize: "cover",
@@ -14,8 +15,8 @@ export default function Home({ hero1Url, hero2Url, hero3Url }) {
         <div className="h-full w-full bg-black backdrop-blur-sm bg-opacity-50 hover:backdrop-blur-none text-white font-karla flex flex-col text-3xl items-center justify-center">
           <p className="w-full text-center m-3 text-[3rem] font-permanent-marker p-12 leading-normal">Explore Our Designs</p>
         </div>
-      </div></a>
-      <a href='/connect'><div
+      </div></Link>
+      <Link href='/connect'><div
         style={{
           backgroundImage: `url(${`${hero2Url}?blur=50`})`,
           backgroundSize: "cover",
@@ -26,8 +27,8 @@ export default function Home({ hero1Url, hero2Url, hero3Url }) {
         <div className="h-full w-full bg-black backdrop-blur-sm bg-opacity-50 hover:backdrop-blur-none text-white font-karla flex flex-col text-3xl items-center justify-center">
           <p className="w-full text-center m-3 text-[3rem] font-permanent-marker p-12 leading-normal">Custom Screen Printing</p>
         </div>
-      </div></a>
-      <a href='/store'><div
+      </div></Link>
+      <Link href='/store'><div
         style={{
           backgroundImage: `url(${`${hero3Url}?blur=50`})`,
           backgroundSize: "cover",
@@ -38,7 +39,7 @@ export default function Home({ hero1Url, hero2Url, hero3Url }) {
           <p className="w-full text-center m-3 text-[3rem] font-permanent-marker p-12 leading-normal">Check Out Our Merch</p>
           {/* <button className="hidden md:block w-3/6 bg-burnt-pink text-white py-2 px-6 rounded-lg m-3  tracking-wider border-2 border-white hover:rotate-12 uppercase italic font-permanent-marker">Test</button> */}
         </div>
-      </div></a>
+      </div></Link>
     </div>
   );
 }
