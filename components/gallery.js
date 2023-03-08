12d202587997c2bef16f5fc610afa5ee45c72fea
keegan-olson <76485182+keegan-olson@users.noreply.github.com>
2023-03-08 14:48:20 -0800

    card updates

--- components/gallery.js
+++ components/gallery.js
@@ -11,31 +11,13 @@ export default function Gallery({ artworks, }) {
         >
           <div className="flex flex-1 flex-col p-6">
           <i class="fa-regular fa-heart self-end fa-xl"></i>
-            <img className="mx-auto h-2/3 w-2/3 flex-shrink-0 rounded" src={urlFor(art.image).url()} alt={art.altText} />
+            <img className="mt-3 mx-auto h-max w-max flex-shrink-0 rounded" src={urlFor(art.image).url()} alt={art.altText} />
             <h3 className="mt-6 text-sm font-medium text-gray-900">{art.title}</h3>
             <dl className="mt-1 flex flex-grow flex-col justify-between">
               <dt className="sr-only">Title</dt>
               <dd className="text-sm text-gray-500">{art.title}</dd>
               <dt className="sr-only">Role</dt>
-              <dd className="mt-3">
-                <span className="rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-800">
-                  temporary
-                </span>
-              </dd>
             </dl>
-          </div>
-          <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
-              <div className="flex w-0 flex-1">
-                <a
-                  href=''
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
-                >
-                  <i class="fa-light fa-envelope"></i>
-                  Email
-                </a>
-              </div>
-            </div>
           </div>
         </li>
       ))}
