e8db342e1c04f7e8bd44fbb90b2495a1e6b5d961
keegan-olson <76485182+keegan-olson@users.noreply.github.com>
2023-03-08 14:28:51 -0800

    bug fixes to gallery

--- components/gallery.js
+++ components/gallery.js
@@ -20,7 +20,7 @@ export default function Gallery({ artworks, }) {
               <dt className="sr-only">Role</dt>
               <dd className="mt-3">
                 <span className="rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-800">
-                  "temporary"
+                  temporary
                 </span>
               </dd>
             </dl>
@@ -29,7 +29,7 @@ export default function Gallery({ artworks, }) {
             <div className="-mt-px flex divide-x divide-gray-200">
               <div className="flex w-0 flex-1">
                 <a
-                  href={`mailto:${art.artist.email}`}
+                  href=''
                   className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
                 >
                   <EnvelopeIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
